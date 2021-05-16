abbf4ca62846759430981f2a906b41e7d7c7b5dd
Felipe Domingues <72865567+NucleoEF-1ano@users.noreply.github.com>
2021-05-16 16:53:05 -0300

    test for no explicit jokes

--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,7 @@
 export const filterJokesByCategories = (jokes, category) =>
 	jokes && jokes.filter((joke) => joke.categories.includes(category));
-
-export const jokesWithoutCategory = (jokes) => jokes && jokes.filter((joke) => joke.categories.length === 0);
+	
+	export const jokesWithoutCategory = (jokes) => jokes && jokes.filter((joke) => joke.categories.length === 0);
+	
+	export const noExplicitJokes = (jokes) =>
+		jokes && jokes.filter((joke) => !joke.categories.includes('explicit'));
--- src/functions.test.js
+++ src/functions.test.js
@@ -1,4 +1,4 @@
-import { filterJokesByCategories, jokesWithoutCategory } from './functions';
+import { filterJokesByCategories, jokesWithoutCategory, noExplicitJokes } from './functions';
 const jokes = [
 	{
 		categories: [ 'explicit' ],
@@ -92,7 +92,8 @@ describe('return only jokes without category', () => {
 					id: 2,
 					joke:
 						'MacGyver can build an airplane out of gum and paper clips. Chuck Norris can kill him and take it.'
-				}
+				},
+			
 			])
 		);
 	});
@@ -101,5 +102,50 @@ describe('return only jokes without category', () => {
 	});
 	it('no match jokes if jokes is empty', () => {
 		expect(jokesWithoutCategory([])).toEqual(expect.arrayContaining([]));
+	});
+});
+
+describe('return no explicit jokes', () => {
+	it('fail search no explicit ', () => {
+		expect(noExplicitJokes(jokes)).toEqual(
+			expect.not.arrayContaining([
+				{
+					categories: [ 'explicit' ],
+					id: 1,
+					joke: 'Chuck Norris uses ribbed condoms inside out, so he gets the pleasure.'
+				},
+
+				{
+					categories: [],
+					id: 2,
+					joke:
+						'MacGyver can build an airplane out of gum and paper clips. Chuck Norris can kill him and take it.'
+				},
+
+				{
+					categories: [ 'nerdy' ],
+					id: 3,
+					joke: 'Chuck Norris can download emails with his pick-up.'
+				}
+			])
+		);
+	});
+	it('Dont match explicit jokes', () => {
+		expect(noExplicitJokes(jokes)).toEqual(
+			expect.arrayContaining([
+				{
+					categories: [],
+					id: 2,
+					joke:
+						'MacGyver can build an airplane out of gum and paper clips. Chuck Norris can kill him and take it.'
+				}
+			])
+		);
+	});
+	it('no match jokes if jokes is null', () => {
+		expect(noExplicitJokes(null)).toEqual(expect.arrayContaining([]));
+	});
+	it('no match jokes if jokes is empty', () => {
+		expect(noExplicitJokes([])).toEqual(expect.arrayContaining([]));
 	});
 });
