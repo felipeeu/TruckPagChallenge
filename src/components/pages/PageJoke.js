9a3a1a426271c25f7f6cee27b066985aa312330c
Felipe Domingues <72865567+NucleoEF-1ano@users.noreply.github.com>
2021-05-16 23:57:46 -0300

    insert loader

--- src/components/pages/PageJoke.js
+++ src/components/pages/PageJoke.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Layout from './Layout';
+import Loader from '../common/Loader';
 
 const PageJoke = ({ jokes, title }) => {
 	return (
 		<Fragment>
 			<CssBaseline />
 			<h1>{title}</h1>
-			<Layout jokes={jokes} />
+			{jokes.length > 0 ? <Layout jokes={jokes} /> : <Loader />}
 		</Fragment>
 	);
 };
